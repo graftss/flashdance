0e8390cbaeb934b93fa1c47d3dd183fa5be3c60d
Nick Fireman <nfireman1@gmail.com>
2017-11-26 03:49:25 -0500

    now it's working i guess

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import 'phaser';
 
 import * as Phaser from 'phaser-ce';
 
+const create = () => { console.log('creating') };
+const preload = () => { console.log('preloading') };
 const update = () => { console.log('updating') };
 
-const game = new Phaser.Game(800, 600, Phaser.AUTO, { update: update });
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload, create, update });
 
