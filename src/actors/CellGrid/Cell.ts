dfe3522d0f1768367dfd2b5595d0b6dd2966d82b
Nick Fireman <nfireman1@gmail.com>
2017-12-01 18:06:08 -0500

    separate `CellInputLayer` from `Cell`

--- src/actors/CellGrid/Cell.ts
+++ src/actors/CellGrid/Cell.ts
@@ -2,7 +2,7 @@ import * as Phaser from 'phaser-ce';
 
 import CellGrid from './CellGrid';
 import FlashLayer from './FlashLayer';
-import Hitbox from '../Hitbox';
+import CellInputLayer from './CellInputLayer';
 import Game from '../..';
 import { isEqual, labelArgs, shiftAnchor } from '../../utils';
 
@@ -12,15 +12,13 @@ const fakeFlashColor = 0xff0000;
 export default class Cell extends Phaser.Group {
   private flashLayer: FlashLayer;
   private fakeFlashLayer: FlashLayer;
-  private hitbox: Hitbox;
-  private inputLayer: Phaser.Sprite;
+  private inputLayer: CellInputLayer;
 
   public inputTarget: InputTarget;
-  private lastDragTarget: InputTarget;
 
   constructor(
     public game: Game,
-    private grid: CellGrid,
+    public parentGrid: CellGrid,
     public x: number,
     public y: number,
     private w: number,
@@ -28,26 +26,17 @@ export default class Cell extends Phaser.Group {
     private col: number,
     private row: number,
   ) {
-    super(game, grid);
+    super(game, parentGrid);
+
+    this.inputTarget = { type: 'cell', cell: { col, row } };
 
     this.flashLayer = new FlashLayer(game, this, w, h, { color: flashColor });
     this.fakeFlashLayer = new FlashLayer(game, this, w, h, { color: fakeFlashColor });
-    this.hitbox = new Hitbox(game, this, 0, 0, w, h);
-
-    this.attachEventHandlers();
-    this.initInputLayer();
-
-    this.inputTarget = { type: 'cell', cell: { col, row } };
-  }
-
-  private attachEventHandlers(): void {
-    const { eventBus } = this.game;
-
-    eventBus.inputEnabled.add(this.setInputEnabled);
+    this.inputLayer = new CellInputLayer(game, this, w, h, this.inputTarget);
   }
 
   public containsPoint(x: number, y: number): boolean {
-    return this.hitbox.containsPoint(x, y);
+    return this.inputLayer.containsPoint(x, y);
   }
 
   public flash = (opts: FlashOpts): TweenWrapper => {
@@ -59,61 +48,4 @@ export default class Cell extends Phaser.Group {
   public fakeFlash = (opts: FlashOpts): TweenWrapper => {
     return this.fakeFlashLayer.flashTween(opts.duration);
   };
-
-  private initInputLayer(): void {
-    this.inputLayer = this.getInputLayerSprite();
-    this.addInputHandlers(this.inputLayer);
-  }
-
-  private setInputEnabled = (enabled: boolean): void => {
-    this.inputLayer.inputEnabled = enabled;
-  };
-
-  // Only sprites can use Phaser's drag events, so we have to convert a
-  // transparent graphic to a sprite via the graphics' texture.
-  private getInputLayerSprite(): Phaser.Sprite {
-    const texture = this.hitbox.generateTexture();
-    return this.game.add.sprite(0, 0, texture, undefined, this);
-  }
-
-  private addInputHandlers(sprite: Phaser.Sprite): void {
-    // setting `inputEnabled` to true initializes `sprite.input`, because
-    // modularity is an illusion
-    sprite.inputEnabled = true;
-
-    sprite.input.enableDrag();
-    sprite.events.onInputDown.add(this.onInputDown);
-    sprite.events.onDragUpdate.add(this.onDragUpdate);
-    sprite.events.onDragStop.add(this.onDragStop);
-
-    sprite.inputEnabled = false;
-  }
-
-  private onInputDown = (): void => {
-    this.lastDragTarget = this.inputTarget;
-    this.game.eventBus.inputDown.dispatch(this.inputTarget);
-  };
-
-  private onDragUpdate = (_, pointer: Phaser.Pointer): void => {
-    const { x, y } = pointer;
-    const cell = this.grid.cellContainingPoint(x, y);
-
-    // for now, ignore drags that take place outside of the grid. maybe
-    // in the future this will change, or maybe having to drag outside of
-    // the grid will be a feature, or maybe ill kill myself before it matters
-    if (cell === null) return;
-
-    const { inputTarget } = cell;
-
-    if (!isEqual(inputTarget, this.lastDragTarget)) {
-      this.lastDragTarget = inputTarget;
-      this.game.eventBus.inputDragTarget.dispatch(inputTarget);
-    }
-  };
-
-  private onDragStop = (): void => {
-    this.game.eventBus.inputDragStop.dispatch(this.lastDragTarget);
-    this.lastDragTarget = null;
-    this.inputLayer.position.copyFrom(this.hitbox.getPosition());
-  }
 }
---
+++ src/actors/CellGrid/CellInputLayer.ts
@@ -0,0 +1,76 @@
+import * as Phaser from 'phaser-ce';
+
+import Cell from './Cell';
+import Hitbox from '../Hitbox';
+import Game from '../..';
+import { isEqual } from '../../utils';
+
+export default class CellInputLayer {
+  private hitbox: Hitbox;
+  private sprite: Phaser.Sprite;
+
+  private lastDragTarget: InputTarget;
+
+  constructor(
+    private game: Game,
+    private parentCell: Cell,
+    private w: number,
+    private h: number,
+    private inputTarget: InputTarget,
+  ) {
+    this.hitbox = new Hitbox(game, parentCell, 0, 0, w, h);
+
+    const texture = this.hitbox.generateTexture();
+
+    this.sprite = game.add.sprite(0, 0, texture, undefined, parentCell);
+
+    // enabling input is what creates the `sprite.input` object, because of
+    // (i assume) shining galaxy brain optimization reasons. so we need to
+    // enable input before enabling drag, and then we disable input because we
+    // never actually wanted input enabled in the first place
+    this.setInputEnabled(true);
+    this.sprite.input.enableDrag();
+    this.setInputEnabled(false);
+
+    this.sprite.events.onInputDown.add(this.onInputDown);
+    this.sprite.events.onDragUpdate.add(this.onDragUpdate);
+    this.sprite.events.onDragStop.add(this.onDragStop);
+
+    game.eventBus.inputEnabled.add(this.setInputEnabled);
+  }
+
+  private setInputEnabled = (enabled: boolean): void => {
+    this.sprite.inputEnabled = enabled;
+  };
+
+  private onInputDown = (): void => {
+    this.lastDragTarget = this.inputTarget;
+    this.game.eventBus.inputDown.dispatch(this.inputTarget);
+  };
+
+  private onDragUpdate = (_, pointer: Phaser.Pointer): void => {
+    const { x, y } = pointer;
+    const cell = this.parentCell.parentGrid.cellContainingPoint(x, y);
+
+    // for now, ignore drags that take place outside of the grid. maybe
+    // in the future this will change, or maybe having to drag outside of
+    // the grid will be a feature, or maybe ill kill myself before it matters
+    if (cell === null) return;
+
+    const { inputTarget } = cell;
+
+    if (!isEqual(inputTarget, this.lastDragTarget)) {
+      this.lastDragTarget = inputTarget;
+      this.game.eventBus.inputDragTarget.dispatch(inputTarget);
+    }
+  };
+
+  private onDragStop = (): void => {
+    this.game.eventBus.inputDragStop.dispatch(this.lastDragTarget);
+    this.sprite.position.copyFrom(this.hitbox.getPosition());
+  };
+
+  public containsPoint(x: number, y: number): boolean {
+    return this.hitbox.containsPoint(x, y);
+  }
+}
