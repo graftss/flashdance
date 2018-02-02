58e2e923432462f7a7c6fb8640b2bab9c7d1561f
Nick Fireman <nfireman1@gmail.com>
2018-02-02 17:32:13 -0500

    add `newRandomLight`

--- src/states/MainMenu/Background.ts
+++ src/states/MainMenu/Background.ts
@@ -3,11 +3,14 @@ import * as Phaser from 'phaser-ce';
 import Array2D from '../../Array2D';
 import BackgroundLight from './BackgroundLight';
 import Game from '../../Game';
+import { random } from '../../utils';
 
 export default class Background extends Phaser.Group {
-  private cellSize: number = 15;
+  private cellSize: number = 200;
   private cellMargin: number = 3;
 
+  private cols: number;
+  private rows: number;
   private grid: Array2D<number>;
   private lightTexture: Phaser.RenderTexture;
 
@@ -19,20 +22,18 @@ export default class Background extends Phaser.Group {
     this.initGrid();
     this.initLightTexture();
 
-    this.newLight(0, 0);
-    this.newLight(0, 1);
-    this.newLight(0, 2);
+    setInterval(() => this.newRandomLight(), 350);
   }
 
   private initGrid(): void {
-    const cellCols = Math.floor(this.game.width / (this.cellSize + this.cellMargin));
-    const cellRows = Math.floor(this.game.height / (this.cellSize + this.cellMargin));
-    this.grid = new Array2D(cellCols, cellRows);
+    this.cols = Math.floor(this.game.width / (this.cellSize + this.cellMargin));
+    this.rows = Math.floor(this.game.height / (this.cellSize + this.cellMargin));
+    this.grid = new Array2D(this.cols, this.rows);
   }
 
   private initLightTexture() {
     const graphic = this.game.add.graphics(0, 0)
-      .beginFill(0xffffff)
+      .beginFill(0xff0000, 0.4)
       .drawRoundedRect(0, 0, this.cellSize, this.cellSize, this.cellSize / 5)
       .endFill();
 
@@ -49,11 +50,27 @@ export default class Background extends Phaser.Group {
     this.grid.set(value, col, row);
   }
 
-  private newLight(col: number, row: number): void {
+  private newLight(col: number, row: number): BackgroundLight {
     const { cellMargin, cellSize, game, lightTexture } = this;
     const x = cellMargin + col * (cellMargin + cellSize);
     const y = cellMargin + row * (cellMargin + cellSize);
 
-    const light = new BackgroundLight(game, x, y, lightTexture);
+    this.setCellUse(1, col, row);
+
+    return new BackgroundLight(game, x, y, lightTexture);
+  }
+
+  private newRandomLight(): Maybe<BackgroundLight> {
+    const maxTries = 3;
+
+    for (let n = 0; n < maxTries; n++) {
+      const col = random(0, this.cols - 1);
+      const row = random(0, this.rows - 1);
+      if (!this.cellInUse(col, row)) {
+        return this.newLight(col, row);
+      }
+    }
+
+    return null;
   }
 }
