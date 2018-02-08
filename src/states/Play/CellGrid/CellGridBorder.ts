c0ce8325b3a8fcd549256cc9e521b779843cfa33
Nick Fireman <nfireman1@gmail.com>
2018-02-08 18:48:41 -0500

    add `CellGridBorder` animations

--- src/states/Play/CellGrid/CellGridBorder.ts
+++ src/states/Play/CellGrid/CellGridBorder.ts
@@ -1,14 +1,12 @@
 import * as Phaser from 'phaser-ce';
 
 import Game from '../../../Game';
-import { destroy } from '../../../utils';
+import { destroy, shiftAnchor } from '../../../utils';
 
 export default class CellGridBorder extends Phaser.Group {
   private border: Phaser.Graphics;
-  private orientor: Phaser.Group;
-  private topMarker: Phaser.Graphics;
-  private bottomMarker: Phaser.Graphics;
-  private showTop: boolean = true;
+  private pulseTimeout: any;
+  private borderTween: Phaser.Tween;
 
   constructor(
     public game: Game,
@@ -21,7 +19,10 @@ export default class CellGridBorder extends Phaser.Group {
   ) {
     super(game, parent);
 
+    shiftAnchor(this, this.w / 2, this.h / 2);
     this.initBorder();
+
+    this.game.eventBus().inputEnabled.add(this.onInputEnabled);
   }
 
   private initBorder(): void {
@@ -34,4 +35,38 @@ export default class CellGridBorder extends Phaser.Group {
       .drawRect(0, 0, w + 2 * thickness, h + 2 * thickness);
   }
 
+  private startPulse(): void {
+    this.pulseTimeout = setInterval(this.pulse, 1300);
+  }
+
+  private stopPulse(): void {
+    clearInterval(this.pulseTimeout);
+  }
+
+  private pulse = (): void => {
+    const { chain, scale } = this.game.tweener;
+
+    chain([scale(this, 1.01, 100), scale(this, 1, 100)]).start();
+  }
+
+  private onInputEnabled = (enabled: boolean): void => {
+    const { tint } = this.game.tweener;
+
+    if (enabled) {
+      this.startBorderTween(tint(this.border, 0x00ff00, 200));
+      this.startPulse();
+    } else {
+      this.startBorderTween(tint(this.border, 0xffffff, 200));
+      this.stopPulse();
+    }
+  }
+
+  private startBorderTween(tween: Phaser.Tween): void {
+    if (this.borderTween) {
+      this.borderTween.stop();
+    }
+
+    this.borderTween = tween;
+    this.borderTween.start();
+  }
 }
